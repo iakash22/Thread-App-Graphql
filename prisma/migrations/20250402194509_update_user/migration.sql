bf9a1935570c950a2d4386d6a42dfeceb99271fe
Akash kumar <94979388+iakash22@users.noreply.github.com>
2025-04-03 01:22:21 +0530

    setup and connected prisma

---
+++ prisma/migrations/20250402194509_update_user/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "first_name" DROP NOT NULL,
+ALTER COLUMN "last_name" DROP NOT NULL,
+ALTER COLUMN "profile_image_url" DROP NOT NULL,
+ALTER COLUMN "email" DROP NOT NULL,
+ALTER COLUMN "password" DROP NOT NULL,
+ALTER COLUMN "salt" DROP NOT NULL;
